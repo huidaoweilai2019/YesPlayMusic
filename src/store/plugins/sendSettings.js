2ca1f51a17a10ce0009ec4ef5ec961c0cb7bd2c3
huidaoweilai2019 <46296150+huidaoweilai2019@users.noreply.github.com>
2021-04-07 03:44:40 +0800

    Add files via upload

--- src/store/plugins/sendSettings.js
+++ src/store/plugins/sendSettings.js
@@ -7,8 +7,6 @@ export function getSendSettingsPlugin() {
       if (mutation.type !== "updateSettings") return;
       ipcRenderer.send("settings", {
         minimizeToTray: state.settings.minimizeToTray,
-        enableGlobalShortcut: state.settings.enableGlobalShortcut,
-        appearance: state.settings.appearance,
       });
     });
   };
